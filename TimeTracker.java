3c7d774c3a21901ca884de80dfaf388644362f95
quentinrh12 <quentinrh12@gmail.com>
2021-09-24 17:16:48 -0500

    full program

--- TimeTracker.java
+++ TimeTracker.java
@@ -1,6 +1,6 @@
 
 import java.util.ArrayList;
-// import java.util.Hashtable;
+import java.util.Hashtable;
 
 public class TimeTracker {
     public static void main(String args[]){
@@ -13,37 +13,104 @@ public class TimeTracker {
             addMiddle(list, j);
             addEnd(list, j);
 
-            for (int i=0; i==j-1; i++){
+            for (int i=0; i<=j-1; i++){
                 list.add(i, i);
             }
-
             dltFront(list, j);
+
+            for (int i=0; i<=j-1; i++){
+                list.add(i, i);
+            }
             dltMiddle(list, j);
+
+            for (int i=0; i<=j-1; i++){
+                list.add(i, i);
+            }
             dltEnd(list, j);
         
+            Hashtable<Integer, Integer> dict = new Hashtable<Integer, Integer>();
+            
+            addDict(dict, j);
+            dltDict(dict, j);
 
-        //Hashtable<String, Integer> dict = new Hashtable<String, Integer>();
-        //TimeTracker timer2 = new TimeTracker(10, dict);
         }        
+    }
+
+    private static void addDict(Hashtable<Integer, Integer> dict, int size){
+        double totalTime = 0;
+
+        for (int i=1; i<=10; i++){
+
+            double start = System.nanoTime();
+
+            for (int j=1; j<=size; j++){
+                dict.put(j, j);
+            }
+
+            double stop = System.nanoTime();
+
+            dict.clear();
+
+            double elapsedNanoSec = stop - start;
+            double elapsedTime = elapsedNanoSec / 1000000000;
+            double avgElapsed = elapsedTime / size;
+            totalTime = totalTime + avgElapsed;
+
+        }
+
+        double avgTime = totalTime / 10;
+
+        System.out.println("Adding to Dictionary for Size " + size + ":");
+        System.out.println("     Average Elapsed Time = " + avgTime + " seconds");
+    }
+
+    private static void dltDict(Hashtable<Integer, Integer> dict, int size){
+        double totalTime = 0;
+
+        for (int i=1; i<=10; i++){
+
+            for (int j=1; j<=size; j++){
+                dict.put(j, j);
+            }
+
+            double start = System.nanoTime();
+
+            for (int k=size; k>=1; k--){
+                dict.remove(k);
+            }
+
+            double stop = System.nanoTime();
+
+            double elapsedNanoSec = stop - start;
+            double elapsedTime = elapsedNanoSec / 1000000000;
+            double avgElapsed = elapsedTime / size;
+            totalTime = totalTime + avgElapsed;
+
+        }
+
+        double avgTime = totalTime / 10;
+
+        System.out.println("Deleting from Dictionary for Size " + size + ":");
+        System.out.println("     Average Elapsed Time = " + avgTime + " seconds");
     }
 
     private static void addFront(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int i=1; i==10; i++){
+        for (int i=1; i<=10; i++){
 
             double start = System.nanoTime();
     
-            for (int j=1; j==size; j++){
+            for (int j=1; j<=size; j++){
                 list.add(0, j);
             }
 
             double stop = System.nanoTime();
 
-            for (int k=0; k<list.size(); k++){
+            for (int k=0; k<=list.size()-1; k++){
                 list.remove(k);
             }
-
+            
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
             double avgElapsed = elapsedTime / size;
@@ -59,16 +126,20 @@ public class TimeTracker {
     private static void addMiddle(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int i=1; i==10; i++){
+        for (int i=1; i<=10; i++){
 
             int mid = size/2;
             double start = System.nanoTime();
             
-            for (int j=1; j==size; j++){
+            for (int j=1; j<=size; j++){
                 list.add(mid, j);
             }
 
             double stop = System.nanoTime();
+
+            for (int k=0; k<=list.size()-1; k++){
+                list.remove(k);
+            }
 
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
@@ -84,15 +155,19 @@ public class TimeTracker {
     private static void addEnd(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int i=1; i==10; i++){
+        for (int i=1; i<=10; i++){
 
             double start = System.nanoTime();
             
-            for (int j=1; j==size; j++){
+            for (int j=1; j<=size; j++){
                 list.add(size-1, j);
             }
 
             double stop = System.nanoTime();
+
+            for (int k=0; k<=list.size()-1; k++){
+                list.remove(k);
+            }
 
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
@@ -109,15 +184,19 @@ public class TimeTracker {
     private static void dltFront(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int j=1; j==10; j++){
+        for (int j=1; j<=10; j++){
             
             double start = System.nanoTime();
             
-            for (int i=0; i==size-1; i++){
+            for (int i=0; i<=size-1; i++){
                 list.remove(0);
             }
     
             double stop = System.nanoTime();
+
+            for (int k=0; k<=size-1; k++){
+                list.add(k, k);
+            }
 
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
@@ -134,15 +213,19 @@ public class TimeTracker {
     private static void dltMiddle(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int j=1; j==10; j++){
+        for (int j=1; j<=10; j++){
             
             double start = System.nanoTime();
             
-            for (int i=0; i==size-1; i++){
+            for (int i=0; i<=size-1; i++){
                 list.remove(list.size()/2);
             }
     
             double stop = System.nanoTime();
+
+            for (int k=0; k<=size-1; k++){
+                list.add(k, k);
+            }
 
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
@@ -159,15 +242,19 @@ public class TimeTracker {
     private static void dltEnd(ArrayList<Integer> list, int size){
         double totalTime = 0;
 
-        for (int j=1; j==10; j++){
+        for (int j=1; j<=10; j++){
             
             double start = System.nanoTime();
             
-            for (int i=0; i==size-1; i++){
-                list.remove(list.size());
+            for (int i=0; i<=size-1; i++){
+                list.remove(list.size()-1);
             }
     
             double stop = System.nanoTime();
+
+            for (int k=0; k<=size-1; k++){
+                list.add(k, k);
+            }
 
             double elapsedNanoSec = stop - start;
             double elapsedTime = elapsedNanoSec / 1000000000;
